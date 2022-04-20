d2ae65e726c9765f59282166dc6c0739c2ec6a36
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-04-20 18:05:41 -0400

    added templates to app/auth, dashboard, log, manage account, profile_edit, profile_view, register, user_edit and user_new

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,13 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
 
 from app.auth import auth
-from app.auth import auth
 from app.auth.admin import admin
 from app.cli import create_database, create_log_folder
 from app.context_processors import utility_text_processors
 from app.db import db
 from app.db.models import User
 from app.error_handlers import error_handlers
-from app.logging_config import log_con
+from app.logging_configuration import log_con
 from app.simple_pages import simple_pages
 
 login_manager = flask_login.LoginManager()
@@ -43,11 +42,12 @@ def create_app():
     # these load functions with web interface
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
+    app.register_blueprint(admin)
 
     # these load functionality without a web interface
     app.register_blueprint(log_con)
     app.register_blueprint(error_handlers)
-    app.register_blueprint(admin)
+
     app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
