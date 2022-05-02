91a78162d91ef25106e2098a4ff42d68fa27e0c8
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-05-02 15:43:05 -0400

    added SIMPLE PAGES TESTS and few more routes tests

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -2,19 +2,19 @@
 
 def test_request_main_menu_links(client):
     """This makes the index page"""
-    response = client.get("/")
+    response = client.get("/home")
     assert response.status_code == 200
     assert b'href="/about"' in response.data
-    #assert b'href="/welcome"' in response.data
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
 
+
 def test_request_index(client):
     """This makes the index page"""
-    response = client.get("/")
+    response = client.get("/home")
     assert response.status_code == 200
-    assert b"Organize Your Music" in response.data
+    assert b"Easiest Banking." in response.data
 
 def test_request_about(client):
     """This makes the index page"""
@@ -27,6 +27,30 @@ def test_request_about(client):
 #  response = client.get("/welcome")
 #   assert response.status_code == 200
 #    assert b"welcome" in response.data
+
+
+def test_request_dashboard(client):
+    """This tests dashboard """
+    # redirect to /login since not authenticated
+    response = client.get("/dashboard", follow_redirects=True)
+    assert response.status_code == 200
+    assert b"If you do not have an account, please" in response.data
+
+
+def test_request_login(client):
+    """This tests the login """
+    response = client.get("/login")
+    assert response.status_code == 200
+    assert b"If you do not have an account, please" in response.data
+
+
+def test_request_registration(client):
+    """ this tests /registration """
+    response = client.get("/register")
+    assert response.status_code == 200
+    assert b"Register<" in response.data
+
+
 
 def test_request_page_not_found(client):
     """This makes the index page"""
