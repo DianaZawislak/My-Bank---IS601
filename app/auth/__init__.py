3db6eace388fa19c7c8863b2ed8010874d48a440
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-05-03 20:55:26 -0400

    fixed routing issues

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -39,12 +39,12 @@ def register():
 def register_login():
     form = login_form()
     if current_user.is_authenticated:
-        return redirect(url_for('auth.dashboard'))
+        return redirect(url_for('auth.index'))
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('auth.register'))
+            return redirect(url_for('auth.register_login'))
         else:
             user.authenticated = True
             db.session.add(user)
@@ -52,7 +52,29 @@ def register_login():
             login_user(user)
             flash("Welcome", 'success')
             return redirect(url_for('auth.index'))
-    return render_template('login.html', form=form)
+    return render_template('register_login.html', form=form)
+def register1():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.dashboard'))
+    form = register_form()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = User(email=form.email.data, password=generate_password_hash(form.password.data))
+            db.session.add(user)
+            db.session.commit()
+            if user.id == 1:
+                user.is_admin = 1
+                db.session.add(user)
+                db.session.commit()
+                flash('Congratulations !!! You are successfully registered, please log in', 'success')
+            # flash('Congratulations, you are now a registered user, please', <a href="{{ url_for('auth.login', page="login_form")}}">log in</a>., "success")
+            return redirect(url_for('auth.register_login'), 302)
+        else:
+                flash('Already Registered')
+                return redirect(url_for('auth.register_login'), 302)
+        return render_template('register_login.html', form=form)
+
 
 @auth.route('/login', methods=['POST', 'GET'])
 def login():
@@ -63,7 +85,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('auth.register'))
+            return redirect(url_for('auth.register_login'))
         else:
             user.authenticated = True
             db.session.add(user)
