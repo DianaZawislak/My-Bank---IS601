12db990674c80d5723b12c678e4f74a3f919e4dc
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-05-09 23:18:25 -0400

    fixed upload access granted TEST

--- tests/upload_accessibility_test.py
+++ tests/upload_accessibility_test.py
@@ -6,6 +6,6 @@ def test_upload_denied(client):
     assert response.status_code == 404
 
 
-def test_upload_denied(client):
-    response = client.get("/upload", follow_redirects=False)
-    assert response.status_code == 404
+def test_upload_granted(client):
+    response = client.get("/transactions/upload", follow_redirects=True)
+    assert response.status_code == 200
