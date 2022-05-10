449857e05b3d6e03564dba7d929fea654d5572a8
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-05-09 23:08:01 -0400

    added registration with wrong email format and mismatched paswswords  TESTS

--- tests/registration_test.py
+++ tests/registration_test.py
@@ -1,4 +1,5 @@
-""" Testing registration"""
+""" Testing registration, correct registration, with insufficient password,
+    incorrect email format and mismatched passwords"""
 
 
 def test_registration(client, application):
@@ -15,3 +16,17 @@ def test_registeristration_password_not_satisfied(client):
     response = client.post("/register", data={"email": "diana@test.com", "password": "aaa", "confirm": "aaa"})
     #there should be no redirection
     assert response.status_code == 200
+
+def test_registeristration_email_wrong_format(client):
+    #Testing registering with wrong email format
+    response = client.post("/register", data={"email": "diana", "password": "aaaaa", "confirm": "aaaaa"})
+    #there should be no redirection
+    assert response.status_code == 200
+
+def test_register_with_mismatched_passwords(client):
+    #Testing registration fail when passwords dont match
+    response = client.post("/register", data={"email": "diana@diana.com", "password": "aaaaaa", "confirm": "bbbbbb"},
+                           follow_redirects=True)
+    #no redirection should happen
+    assert response.status_code == 200
+    assert b"Passwords must match" in response.data
