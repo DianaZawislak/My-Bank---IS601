9bebab97da162e89e86d8600d96c94fcb46e684c
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-05-09 22:18:19 -0400

    added password validators to require min 6 character fro both blueprints (admin and auth) added TEST to make sure no redirection to login happens when password is too short at registration

--- tests/registration_test.py
+++ tests/registration_test.py
@@ -9,3 +9,9 @@ def test_registration(client, application):
     }, follow_redirects=True)
     assert response.status_code == 200
     assert response.request.path == '/register_login'
+
+def test_registeristration_password_not_satisfied(client):
+    #Testing registering with password not meeting requirements (min 6 characters)
+    response = client.post("/register", data={"email": "diana@test.com", "password": "aaa", "confirm": "aaa"})
+    #there should be no redirection
+    assert response.status_code == 200
